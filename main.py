7df2f92b6350a02ef904360f3effc15c96e872f3
antonzubikov <124088976+antonzubikov@users.noreply.github.com>
2023-09-18 19:15:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,24 @@ terminal_nodes = df[df['проект'].str.startswith('Data')]
 
 # Итоговый датафрейм с суммами для каждой нетерминальной вершины и годовыми значениями
 result = terminal_nodes.groupby('код').agg({'проект': lambda x: tuple(set(x)), 'проект': 'first', **{year: 'sum' for year in years}}).reset_index()
+print(result)
 
-# Подключение к PostgreSQL и сохранение данных в таблицу
+# # Подключение к PostgreSQL и сохранение данных в таблицу
 engine = create_engine('postgresql://user:password@localhost/postgres_database')
 result.to_sql('projects', engine, index=False, if_exists='replace')
+
+
+# Создание и изменение таблицы вручную:
+
+# import psycopg2
+# 
+# conn = psycopg2.connect(database="postgres_database", user="user", password="password", host="localhost")
+# cursor = conn.cursor()
+# cursor.execute('CREATE TABLE pandas_data (code VARCHAR(10), project VARCHAR(50), "2022" DECIMAL(6), "2023" DECIMAL(6), "2024" DECIMAL(6), "2025" DECIMAL(6))')
+# for index, data in result.iterrows():
+#     cursor.execute('INSERT INTO pandas_data (code, project, "2022", "2023", "2024", "2025") VALUES (%s, %s, %s, %s, %s, %s)',
+#                    (data['код'], data['проект'], data['2022'], data['2023'], data['2024'], data['2025']))
+#     
+# conn.commit()
+# cur.close()
+# conn.close()
